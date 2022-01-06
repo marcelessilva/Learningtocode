3a89b60adf8b9071cff28f8372e89e27fef7feaa
marcelessilva <marcelessilva@gmail.com>
2022-01-06 00:33:42 -0300

    Tratamento de Erros nas variáveis numéricas > Try Catch

--- src/CalculoLucro.java
+++ src/CalculoLucro.java
@@ -6,37 +6,51 @@ import java.text.NumberFormat;
 public class CalculoLucro {
 
     public static void main(String[] args) {
-        String nome;
-        float receita = 0;
-        float despesas = 0;
-        float resultado;
+        try {
+            String nome;
+            float receita = 0;
+            float despesas = 0;
+            float resultado;
 
-        //ler o nome do usuario
-        System.out.println("Por favor preencha seu nome");
-        nome = readTextFromUser();
-        //nome = "Zé";
-        System.out.println("Usuario logado: " + nome);
+            //ler o nome do usuario
+            System.out.println("Por favor preencha seu nome");
+            //nome = readTextFromUser();
+            nome = "Zé";
+            System.out.println("Usuario logado: " + nome);
+            try {
+                System.out.println("Por favor preencha suas receitas (somente números)");
+                //receita = 150000;
+                receita = readDecimalNumberFromUser();
 
-        System.out.println("Por favor preencha suas receitas (somente números)");
-        //receita = 150000;
-        receita = readDecimalNumberFromUser();
+            } catch (Exception e) {
+                System.out.println("Deu Ruim");
+                System.out.println("Por favor preencha suas receitas somente com números");
+                receita = readDecimalNumberFromUser();
+            }
+            try {
+                System.out.println("Por favor preencha suas despesas (somente números)");
+                despesas = readDecimalNumberFromUser();
+                //despesas=100000;
+            } catch (Exception e) {
+                System.out.println("Deu Ruim");
+                System.out.println("Por favor preencha suas despesas somente com números");
+                despesas = readDecimalNumberFromUser();
+            }
+            resultado = receita - despesas;
+            NumberFormat formatter = NumberFormat.getCurrencyInstance();
+            String resultadoformatado = formatter.format(resultado);
 
-        System.out.println("Por favor preencha suas despesas (somente números)");
-        despesas = readDecimalNumberFromUser();
-        //despesas=100000;
-
-        resultado = receita - despesas;
-        NumberFormat formatter = NumberFormat.getCurrencyInstance();
-        String resultadoformatado = formatter.format(resultado);
-
-        if (resultado < 0) {
-            System.out.println("Prejuízo " + resultadoformatado);
-        }
-        if (resultado == 0) {
-            System.out.println("Zero a zero " + resultadoformatado);
-        }
-        if (resultado > 0) {
-            System.out.println("Lucro " + resultadoformatado);
+            if (resultado < 0) {
+                System.out.println("Prejuízo " + resultadoformatado);
+            }
+            if (resultado == 0) {
+                System.out.println("Zero a zero " + resultadoformatado);
+            }
+            if (resultado > 0) {
+                System.out.println("Lucro " + resultadoformatado);
+            }
+        }catch (Exception e) {
+            System.out.println("Tente mais tarde");
         }
 
     }
