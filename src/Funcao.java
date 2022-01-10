f3531c8ef2874c08ed9c6e5412824a6a98c544ef
marcelessilva <marcelessilva@gmail.com>
2022-01-09 23:25:27 -0300

    Função cálculo desconto IR e salário líquido + printar alíquota e os demais

--- src/Funcao.java
+++ src/Funcao.java
@@ -1,3 +1,7 @@
+import java.text.NumberFormat;
+
+import static util.Util.formatCurrency;
+
 public class Funcao {
 
 
@@ -5,10 +9,11 @@ public class Funcao {
 
         //System.out.println(sum(2, 8));
         //printHelloWorld();
-        defineNumberPositiveNegative(10);
-        defineNumberPositiveNegative(-25);
-        defineNumberPositiveNegative(0);
+        //defineNumberPositiveNegative(10);
+        //defineNumberPositiveNegative(-25);
+        //defineNumberPositiveNegative(0);
 
+        calcularValorIR(4000);
 
     }
 
@@ -31,6 +36,38 @@ public class Funcao {
         } else {
             System.out.println("O Número " + numero + " é negativo");
         }
+
+    }
+
+
+    static void calcularValorIR(float salario) {
+
+        float aliquota = 0;
+        float deducao = 0;
+
+        if (salario <= 1903.98) {
+            System.out.println("Não haverá descontos referente ao IR no seu salário.");
+            return;
+        } else if (salario >= 1903.99 && salario <= 2826.65) {
+            aliquota = 7.5f;
+            deducao = 142.80f;
+
+        } else if (salario >= 2826.66 && salario <= 3751.05) {
+            aliquota = 15f;
+            deducao = 354.80f;
+
+        } else if (salario >= 3751.06 && salario <= 4664.68) {
+            aliquota = 22.5f;
+            deducao = 636.13f;
+        } else {
+            aliquota = 27.5f;
+            deducao = 869.36f;
+        }
+        float valorDescontado = salario * aliquota / 100 - deducao;
+        float salarioLiquido = salario - valorDescontado;
+        System.out.println("Alíquota: " + aliquota + "%");
+        System.out.println("Valor Descontado: " + formatCurrency(valorDescontado));
+        System.out.println("Salário Líquido " + formatCurrency(salarioLiquido));
 
     }
 }
