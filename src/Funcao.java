093717ba724dbbcc8a1bb51ddcac185bde0ee28c
marcelessilva <marcelessilva@gmail.com>
2022-01-12 23:38:50 -0300

    Método para calcular desconto INSS, return dentro do IF, algorítimo

--- src/Funcao.java
+++ src/Funcao.java
@@ -13,7 +13,9 @@ public class Funcao {
         //defineNumberPositiveNegative(-25);
         //defineNumberPositiveNegative(0);
 
-        calcularValorIR(4000);
+
+        float descontoINSS = calcularDescontoINSS(5000);
+        System.out.println(formatCurrency(descontoINSS));
 
     }
 
@@ -69,5 +71,47 @@ public class Funcao {
         System.out.println("Valor Descontado: " + formatCurrency(valorDescontado));
         System.out.println("Salário Líquido " + formatCurrency(salarioLiquido));
 
+
+    }
+
+    static float calcularDescontoINSS(float salario) {
+
+        float aliquotaInss = 0f;
+        float valorDescontoMaximoFaixa1 = 90.90f;
+        float valorDescontoMaximoFaixa2 = 109.42f;
+        float valorDescontoMaximoFaixa3 = 145.67f;
+        float valorDescontoMaximoFaixa4 = 482.55f;
+        float valorMinimoFaixa2 = 1212.01f;
+        float valorMinimoFaixa3 = 2427.80f;
+        float valorMinimoFaixa4 = 3641.70f;
+
+        if (salario <= 1212) {
+            aliquotaInss = 7.5f / 100f;
+            return valorDescontoMaximoFaixa1;
+
+        } else if (salario >= 1212.01 && salario <= 2427.79) {
+            aliquotaInss = 9 / 100f;
+            return ((salario - valorMinimoFaixa2) * aliquotaInss) + valorDescontoMaximoFaixa1;
+//retorna o total de o salario - valor minimo faixa 2 * 9% + valor desconto maximo faixa 1
+
+        } else if (salario >= 2427.80 && salario <= 3641.69) {
+            aliquotaInss = 12 / 100f;
+            return ((salario - valorMinimoFaixa3) * aliquotaInss + valorDescontoMaximoFaixa2 + valorDescontoMaximoFaixa1);
+// retorna o total de o salario - valor minimo faixa 3 * 12% + valor desconto máximo faixa 2 + valor desconto maximo faixa 1
+
+        } else if (salario >= 3641.70 && salario <= 7088.50) {
+            aliquotaInss = 14 / 100f;
+            return ((salario - valorMinimoFaixa4) * aliquotaInss) + valorDescontoMaximoFaixa3 + valorDescontoMaximoFaixa2 + valorDescontoMaximoFaixa1;
+// retorna o total do salario - valor minimo faixa 4 *14% + valor desconto maximo faixa 3 + valor desconto máximo faixa 2 + valor desconto maximo faixa 1
+
+        } else {
+// valor desconto maximo faixa 4 + valor desconto maximo faixa 3 + valor desconto máximo faixa 2 + valor desconto maximo faixa 1
+            return valorDescontoMaximoFaixa4 + valorDescontoMaximoFaixa3 + valorDescontoMaximoFaixa2 + valorDescontoMaximoFaixa1;
+        }
+
+    }
+
+    static float calcularSalarioBaseIR(float salario, float descontoINSS) {
+        return salario - descontoINSS;
     }
 }
