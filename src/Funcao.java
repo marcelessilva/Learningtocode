bcb6986ceceb2bf82e7db04ae29fc97d9c0d7b4c
marcelessilva <marcelessilva@gmail.com>
2022-01-14 22:04:50 -0300

    Transformar valor literal, número mágico, em constante na Classe + Método Salário Família

--- src/Funcao.java
+++ src/Funcao.java
@@ -1,8 +1,21 @@
-import java.text.NumberFormat;
-
 import static util.Util.formatCurrency;
 
 public class Funcao {
+
+
+    public static final float LIMITE_SALARIO_FAMILIA = 1503.25f;
+    public static final float SALARIO_FAMILIA = 51.27f;
+    public static final float SALARIO_MINIMO = 1212f;
+    public static final float INSS_MIN_FAIXA_2 = 1212.01f;
+    public static final float INSS_MAX_FAIXA_2 = 2427.79f;
+    public static final float INSS_MIN_FAIXA_3 = 2427.80f;
+    public static final float INSS_MAX_FAIXA_3 = 3641.69f;
+    public static final float INSS_MIN_FAIXA_4 = 3641.70f;
+    public static final float INSS_MAX_FAIXA_4 = 7088.50f;
+    public static final float INSS_ALIQUOTA_FAIXA_1 = 7.5f / 100f;
+    public static final float INSS_ALIQUOTA_FAIXA_2 = 9 / 100f;
+    public static final float INSS_ALIQUOTA_FAIXA_3 = 12 / 100f;
+    public static final float INSS_ALIQUOTA_FAIXA_4 = 14 / 100f;
 
 
     public static void main(String[] args) {
@@ -48,26 +61,24 @@ public class Funcao {
         float deducao = 0;
 
         if (salario <= 1903.98) {
-            System.out.println("Não haverá descontos referente ao IR no seu salário.");
             return;
         } else if (salario >= 1903.99 && salario <= 2826.65) {
-            aliquota = 7.5f;
+            aliquota = 7.5f /100f;
             deducao = 142.80f;
 
         } else if (salario >= 2826.66 && salario <= 3751.05) {
-            aliquota = 15f;
+            aliquota = 15f /100f;
             deducao = 354.80f;
 
         } else if (salario >= 3751.06 && salario <= 4664.68) {
-            aliquota = 22.5f;
+            aliquota = 22.5f / 100f;
             deducao = 636.13f;
         } else {
-            aliquota = 27.5f;
+            aliquota = 27.5f /100f;
             deducao = 869.36f;
         }
-        float valorDescontado = salario * aliquota / 100 - deducao;
+        float valorDescontado = salario * aliquota - deducao;
         float salarioLiquido = salario - valorDescontado;
-        System.out.println("Alíquota: " + aliquota + "%");
         System.out.println("Valor Descontado: " + formatCurrency(valorDescontado));
         System.out.println("Salário Líquido " + formatCurrency(salarioLiquido));
 
@@ -75,43 +86,35 @@ public class Funcao {
     }
 
     static float calcularDescontoINSS(float salario) {
+        final float valorDescontoMaximoFaixa1 = SALARIO_MINIMO * INSS_ALIQUOTA_FAIXA_1;
+        final float valorDescontoMaximoFaixa2 = INSS_MAX_FAIXA_2 * INSS_ALIQUOTA_FAIXA_2;
+        final float valorDescontoMaximoFaixa3 = INSS_MAX_FAIXA_3 * INSS_ALIQUOTA_FAIXA_3;
+        final float valorDescontoMaximoFaixa4 = INSS_MAX_FAIXA_4 * INSS_ALIQUOTA_FAIXA_4;
 
-        float aliquotaInss = 0f;
-        float valorDescontoMaximoFaixa1 = 90.90f;
-        float valorDescontoMaximoFaixa2 = 109.42f;
-        float valorDescontoMaximoFaixa3 = 145.67f;
-        float valorDescontoMaximoFaixa4 = 482.55f;
-        float valorMinimoFaixa2 = 1212.01f;
-        float valorMinimoFaixa3 = 2427.80f;
-        float valorMinimoFaixa4 = 3641.70f;
-
-        if (salario <= 1212) {
-            aliquotaInss = 7.5f / 100f;
+        if (salario <= SALARIO_MINIMO) {
             return valorDescontoMaximoFaixa1;
-
-        } else if (salario >= 1212.01 && salario <= 2427.79) {
-            aliquotaInss = 9 / 100f;
-            return ((salario - valorMinimoFaixa2) * aliquotaInss) + valorDescontoMaximoFaixa1;
-//retorna o total de o salario - valor minimo faixa 2 * 9% + valor desconto maximo faixa 1
-
-        } else if (salario >= 2427.80 && salario <= 3641.69) {
-            aliquotaInss = 12 / 100f;
-            return ((salario - valorMinimoFaixa3) * aliquotaInss + valorDescontoMaximoFaixa2 + valorDescontoMaximoFaixa1);
-// retorna o total de o salario - valor minimo faixa 3 * 12% + valor desconto máximo faixa 2 + valor desconto maximo faixa 1
-
-        } else if (salario >= 3641.70 && salario <= 7088.50) {
-            aliquotaInss = 14 / 100f;
-            return ((salario - valorMinimoFaixa4) * aliquotaInss) + valorDescontoMaximoFaixa3 + valorDescontoMaximoFaixa2 + valorDescontoMaximoFaixa1;
-// retorna o total do salario - valor minimo faixa 4 *14% + valor desconto maximo faixa 3 + valor desconto máximo faixa 2 + valor desconto maximo faixa 1
-
+        } else if (salario >= INSS_MIN_FAIXA_2 && salario <= INSS_MAX_FAIXA_2) {
+            return ((salario - INSS_MIN_FAIXA_2) * INSS_ALIQUOTA_FAIXA_2) + valorDescontoMaximoFaixa1;
+        } else if (salario >= INSS_MIN_FAIXA_3 && salario <= INSS_MAX_FAIXA_3) {
+            return ((salario - INSS_MIN_FAIXA_3) * INSS_ALIQUOTA_FAIXA_3 + valorDescontoMaximoFaixa2 + valorDescontoMaximoFaixa1);
+        } else if (salario >= INSS_MIN_FAIXA_4 && salario <= INSS_MAX_FAIXA_4) {
+            return ((salario - INSS_MIN_FAIXA_4) * INSS_ALIQUOTA_FAIXA_4) + valorDescontoMaximoFaixa3 + valorDescontoMaximoFaixa2 + valorDescontoMaximoFaixa1;
         } else {
-// valor desconto maximo faixa 4 + valor desconto maximo faixa 3 + valor desconto máximo faixa 2 + valor desconto maximo faixa 1
             return valorDescontoMaximoFaixa4 + valorDescontoMaximoFaixa3 + valorDescontoMaximoFaixa2 + valorDescontoMaximoFaixa1;
         }
-
     }
 
     static float calcularSalarioBaseIR(float salario, float descontoINSS) {
         return salario - descontoINSS;
     }
+
+    static float calcularSalarioFamilia(float salario, int numFilhosElegiveis) {
+        if (salario <= LIMITE_SALARIO_FAMILIA && numFilhosElegiveis > 0) {
+            return numFilhosElegiveis * SALARIO_FAMILIA;
+
+        }
+        return 0;
+    }
+
+
 }
