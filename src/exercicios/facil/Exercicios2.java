2173c93d6a9b509f65e6d3316a40486e5d25d1b2
wiliamjcj <wiljcj@gmail.com>
2022-01-26 20:10:57 -0300

    adicionando exercicios

---
+++ src/exercicios/facil/Exercicios2.java
@@ -0,0 +1,50 @@
+package exercicios.facil;
+
+public class Exercicios2 {
+
+    public static void main(String[] args) {
+
+    }
+
+    /*
+     1 - Faça um algoritmo que leia a idade de uma pessoa expressa em anos, meses e dias e escreva a idade
+      dessa pessoa expressa apenas em dias. Considerar ano com 365 dias e mês com 30 dias.
+     */
+    static void calcularIdadeEmDias(){
+
+    }
+
+    /*
+     2 -   Escreva um programa que pergunte o raio de uma circunferência,
+      e sem seguida mostre o diâmetro, perimetro e área da circunferência
+     */
+    static void calcularMedidasCircunferencia(){
+        double numeroPi = Math.PI;
+
+    }
+
+    /*
+     3 - O álcool tem uma eficiência 30% menor do que a gasolina, baseado nisso faça um programa que leia os preços do litro
+      do álcool e da gasolina e indique ao usuário qual melhor combustível para abastecer o carro.
+     */
+    static void decidirEntreAlcoolGasolina(){
+
+
+    }
+
+    /*
+     4 - Faça um programa que leia um número inteiro do usuário e usando while imprima a sua tabuada.
+     */
+    static void calcularTabuada(){
+
+    }
+
+    /*
+     5 - Usando while faça um programa que leia a renda de todos os integrantes de uma familia,
+      calcule e imprima a renda familiar per capita.
+    */
+    static void calcularRendaPerCapita(){
+
+    }
+
+}
